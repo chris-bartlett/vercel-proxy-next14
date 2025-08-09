046d6a614ae9a2a4f7c5e44c3a620c5dcaaecdb2
Chris Bartlett <chris-bartlett@outlook.com>
2025-08-08 20:08:58 -0400

    using next rewrites

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/my-test-route/:slug",
-        destination: "/another-local-route/:slug", // must be internal
+        destination:
+          "https://vercel-proxy-next15.vercel.app/my-test-route/:slug",
+      },
+      {
+        source: "/_next/:path*",
+        destination: "https://vercel-proxy-next15.vercel.app/_next/:path*",
+      },
+      {
+        source: "/static/:path*",
+        destination: "https://vercel-proxy-next15.vercel.app/static/:path*",
       },
     ];
   },
 };
+
+module.exports = nextConfig;

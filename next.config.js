709a4d26c29ac2816d30ad686ef3289cb32c9080
Chris Bartlett <chris-bartlett@outlook.com>
2025-08-08 20:09:32 -0400

    using next rewrites

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const nextConfig = {
         destination:
           "https://vercel-proxy-next15.vercel.app/my-test-route/:slug",
       },
-      {
-        source: "/_next/:path*",
-        destination: "https://vercel-proxy-next15.vercel.app/_next/:path*",
-      },
-      {
-        source: "/static/:path*",
-        destination: "https://vercel-proxy-next15.vercel.app/static/:path*",
-      },
     ];
   },
 };
